991bede6644335bf32a42ae1497bd0fd51aba1c1
Valentin De Matos <vltn.dematos@gmail.com>
2024-11-24 14:51:31 +0000

    chore: remove deprecated vercel analytics
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
 import FAQ from './pages/FAQ';
 import NotFound from './pages/NotFound';
 import Landing from './pages/Landing';
