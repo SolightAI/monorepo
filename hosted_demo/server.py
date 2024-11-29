abaaf7f12fbfa3b74aeec4f2d9b48a49f4953d4f
Valentin De Matos <vltn.dematos@gmail.com>
2024-11-29 19:36:36 +0100

    fix: putain
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- hosted_demo/server.py
+++ hosted_demo/server.py
@@ -25,7 +25,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://0.0.0.0:8000/ws");
+            var ws = new WebSocket("wss://0.0.0.0:8000/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
