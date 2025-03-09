923cc0b47fc04291b5a8f06427bc5ec87d095cb9
Valentin De Matos <vltn.dematos@gmail.com>
2025-03-09 18:45:23 +0100

    fix(api): typing for test services
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- wapp/api/src/services/test_services.py
+++ wapp/api/src/services/test_services.py
@@ -1,10 +1,10 @@
 from fastapi import HTTPException
 from dto.models import Test as TestModel
-from dto.schemas import Test as TestSchema, TestCreate as TestCreateSchema, TestStatus
+from dto.schemas import TestCreate as TestCreateSchema, TestStatus
 from typing import List
 
 
-async def get_test(test_id: str) -> TestSchema:
+async def get_test(test_id: str) -> TestModel:
     test = await TestModel.get_or_none(id=test_id).prefetch_related("bugs")
 
     if not test:
@@ -13,18 +13,18 @@ async def get_test(test_id: str) -> TestSchema:
     return test
 
 
-async def get_all_tests() -> List[TestSchema]:
+async def get_all_tests() -> List[TestModel]:
     tests = await TestModel.all().prefetch_related("bugs")
     return tests
 
 
-async def create_test(test: TestCreateSchema) -> TestSchema:
+async def create_test(test: TestCreateSchema) -> TestModel:
     test_model = await TestModel.create(**test.model_dump())
 
     return await get_test(test_model.id)  # NOTE: a bit dirty, but it works (prevents issue with ManyToManyField)
 
 
-async def update_test_status(test_id: str, status: TestStatus) -> TestSchema:
+async def update_test_status(test_id: str, status: TestStatus) -> TestModel:
     test = await TestModel.get_or_none(id=test_id)
 
     if not test:
