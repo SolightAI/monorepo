f890fbdf9c8fe5df1f037c46405d193f23d0bde0
Valentin De Matos <vltn.dematos@gmail.com>
2025-03-12 16:44:40 +0100

    fix(api): type hint
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- wapp/api/src/services/epic_services.py
+++ wapp/api/src/services/epic_services.py
@@ -1,10 +1,10 @@
 from fastapi import HTTPException
 from dto.models import Epic as EpicModel
-from dto.schemas import EpicCreate as EpicCreateSchema, Epic as EpicSchema
+from dto.schemas import EpicCreate as EpicCreateSchema
 from pydantic import UUID4
 
 
-async def get_epic(epic_id: UUID4) -> EpicSchema:
+async def get_epic(epic_id: UUID4) -> EpicModel:
     epic = await EpicModel.get_or_none(id=epic_id).prefetch_related("features")
 
     if not epic:
@@ -13,6 +13,6 @@ async def get_epic(epic_id: UUID4) -> EpicSchema:
     return epic
 
 
-async def create_epic(epic: EpicCreateSchema) -> EpicSchema:
+async def create_epic(epic: EpicCreateSchema) -> EpicModel:
     epic_model = await EpicModel.create(**epic.model_dump())
     return await get_epic(epic_model.id)  # NOTE: a bit dirty, but it works (prevents issue with ManyToManyField)
