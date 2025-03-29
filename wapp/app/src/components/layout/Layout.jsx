e441cf7391451e6db498e34f0786c1db98c3480a
evanedreo <evanedreo@gmail.com>
2025-03-28 18:48:21 -0700

    restored the changed ui style

--- wapp/app/src/components/layout/Layout.jsx
+++ wapp/app/src/components/layout/Layout.jsx
@@ -48,7 +48,7 @@ export default function Layout() {
               <div className="ml-2 p-2">
                 <button
                   onClick={handleLogout}
-                  className="text-gray-600 hover:text-gray-900"
+                  className="text-gray-500 hover:text-gray-700"
                 >
                   Logout
                 </button>
