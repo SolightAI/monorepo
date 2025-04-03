30ffeb8e4245735687a3f6187f40ab850f9f4068
Valentin De Matos <vltn.dematos@gmail.com>
2025-04-03 09:46:49 -0700

    chore(test): deprecated file
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- wapp/api/tests/unit/utils/test_basic_utils.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-# Add asyncio marker to make it compatible with the test suite
-@pytest.mark.asyncio 
-async def test_utils_module():
-    """A simple test to verify that the utils module is accessible."""
-    assert True 
