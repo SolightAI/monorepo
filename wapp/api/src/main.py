b8d00ea232b405be75844dd2e97f8f82887958c6
evanedreo <evanedreo@gmail.com>
2025-04-03 01:13:39 -0700

    typo

--- wapp/api/src/main.py
+++ wapp/api/src/main.py
@@ -43,7 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         os.getenv("APP_URL", "http://localhost:3000"),],  
-allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
