6517f9793c6bec6b54dabec72fdef79e19e4e4c4
Valentin De Matos <vltn.dematos@gmail.com>
2025-04-16 18:48:07 -0700

    feat(api): s3 and redis utils
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- wapp/api/src/utils/redis_client.py
+++ wapp/api/src/utils/redis_client.py
@@ -4,11 +4,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT"))
-REDIS_DB = int(os.getenv("REDIS_DB"))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
-
 
 class RedisManager:
     _instance = None
@@ -21,9 +16,20 @@ class RedisManager:
         return cls._instance
 
     def _initialize_client(self):
+
+        try:
+            redis_host = os.getenv("REDIS_HOST")
+            redis_port = int(os.getenv("REDIS_PORT"))
+            redis_db = int(os.getenv("REDIS_DB"))
+            redis_password = os.getenv("REDIS_PASSWORD", None)
+        except Exception as e:
+            logger.error(f"Failed to initialize Redis client: {e}")
+            self._client = None
+            return
+
         if self._client is None:
             # Check if required environment variables are set
-            if not REDIS_HOST or REDIS_PORT is None or REDIS_DB is None:
+            if not redis_host or redis_port is None or redis_db is None:
                 logger.error(
                     "Redis connection cannot be initialized. "
                     "Missing required environment variables: REDIS_HOST, REDIS_PORT, or REDIS_DB."
@@ -34,18 +40,18 @@ class RedisManager:
             try:
                 # decode_responses=True ensures keys/values are returned as strings
                 self._client = redis.Redis(
-                    host=REDIS_HOST,
-                    port=REDIS_PORT,
-                    db=REDIS_DB,
-                    password=REDIS_PASSWORD,
+                    host=redis_host,
+                    port=redis_port,
+                    db=redis_db,
+                    password=redis_password,
                     decode_responses=True,
                     socket_connect_timeout=5  # Add timeout
                 )
                 # Test connection
                 self._client.ping()
-                logger.info(f"Successfully connected to Redis (Singleton) at {REDIS_HOST}:{REDIS_PORT} DB {REDIS_DB}")
+                logger.info(f"Successfully connected to Redis (Singleton) at {redis_host}:{redis_port} DB {redis_db}")
             except redis.exceptions.ConnectionError as e:
-                logger.error(f"Failed to connect to Redis (Singleton) at {REDIS_HOST}:{REDIS_PORT}: {e}")
+                logger.error(f"Failed to connect to Redis (Singleton) at {redis_host}:{redis_port}: {e}")
                 self._client = None  # Ensure client is None if connection fails
             except Exception as e:
                 logger.error(f"An unexpected error occurred during Redis (Singleton) initialization: {e}")
