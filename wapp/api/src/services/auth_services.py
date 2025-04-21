b4ed3c06e4016f1c314bf2a6e6a3035d7add1d3b
albert VALENTIN <avalentin@scaleway.com>
2025-04-21 17:25:14 +0200

    docs(api): add FIXME part to explain comments

--- wapp/api/src/services/auth_services.py
+++ wapp/api/src/services/auth_services.py
@@ -95,6 +95,7 @@ async def get_current_user(token: str) -> UserModel:
             logging.info("No email found in token, returning 401.")
             raise CredentialsException()
         
+        # FIXME: this check doesn't work in test mode
         # token_type = payload.get("token_type")
         # if token_type != "access":
         #     logging.warning("Token is not an access token. It's a " + str(token_type))
