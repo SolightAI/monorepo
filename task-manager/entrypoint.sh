a85e8c553886b55f57198901241f65d26e254b1f
Valentin De Matos <43698357+Thytu@users.noreply.github.com>
2025-04-23 19:31:02 +0200

    feat(task-manager): handles multiple workers (#311)
    * feat(task-manager): multiple workers
    
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>
    
    * feat(task-manager): multiple workers
    
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>
    
    * feat(task-manager): multiple workers
    
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>
    
    ---------
    
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- task-manager/entrypoint.sh
+++ task-manager/entrypoint.sh
@@ -8,5 +8,13 @@ source ./install_dependencies.sh
 
 cd src
 
+# Add current directory to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:.
+
 # Run the application
-exec uvicorn main:app --host 0.0.0.0 --port 8001 --reload
+if [ "${DEV_MODE}" = "true" ]; then
+  echo "Running in development mode with reload"
+  exec fastapi dev --reload main.py --host 0.0.0.0 --port 8001
+else
+  exec fastapi run --workers ${WORKERS:-1} main.py --host 0.0.0.0 --port 8001
+fi
