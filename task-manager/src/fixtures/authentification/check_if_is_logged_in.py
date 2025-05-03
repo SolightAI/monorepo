39e3829658b66aca16ec3db07b4beccfe64ab055
Valentin De Matos <vltn.dematos@gmail.com>
2025-05-03 10:54:20 -0700

    refactor: rename tag to not found and improve app loading
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- task-manager/src/fixtures/authentification/check_if_is_logged_in.py
+++ task-manager/src/fixtures/authentification/check_if_is_logged_in.py
@@ -202,8 +202,8 @@ async def check_is_logged_in(
 
         logger.info(f"[{task_id}] Creating temporary browser context without cookies")
         context = BrowserContext(browser=browser, config=BrowserContextConfig(
-            minimum_wait_page_load_time=1,
-            wait_for_network_idle_page_load_time=2,
+            minimum_wait_page_load_time=3,
+            wait_for_network_idle_page_load_time=3,
         ))
 
         await context.navigate_to(url)
