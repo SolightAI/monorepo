6117e54b0492bf10d92c7744862f51ddc04d812e
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-05-21 18:29:16 +0200

    wapp: update env.example + refacto order test executation status

--- wapp/app/src/utils/testExecutionUtils.js
+++ wapp/app/src/utils/testExecutionUtils.js
@@ -29,28 +29,23 @@ export const EXECUTOR_TYPE = {
  * Helper to sort tests by test execution status
  *
  * @param {string} status - The status of the test execution
- * @returns {number} Object with icon and color for the status
+ * @returns {number} Sort order value for the status
  */
 export const orderStatus = (status) => {
   switch (status) {
     case TEST_STATUS.PASSED:
-      return 0;
     case TEST_STATUS.FAILED:
+    case TEST_STATUS.ERROR:
+    case TEST_STATUS.AGENT_LIMITATION:
+    case TEST_STATUS.NOT_FOUND:
+    case TEST_STATUS.BLOCKED_BY_CAPTCHA:
       return 0;
     case TEST_STATUS.PENDING:
       return 1;
-    case TEST_STATUS.ERROR:
-      return 0;
-    case TEST_STATUS.AGENT_LIMITATION:
-      return 0;
-    case TEST_STATUS.NOT_FOUND:
-      return 0;
-    case TEST_STATUS.BLOCKED_BY_CAPTCHA:
-      return 0;
     case null:
       return 2;
     default:
-      console.log('getStatusInfo', status);
+      console.log('orderStatus', status);
       return 2;
   }
 };
